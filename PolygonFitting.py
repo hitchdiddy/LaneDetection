56e372b8519c6cbf2c5e56b9f16645c731e2b6f5
jkronen <jan@digitalbild-service.de>
2014-02-19 18:31:30 +0100

    update

--- PolygonFitting.py
+++ PolygonFitting.py
@@ -6,16 +6,30 @@ class PolygonFitting:
         pass
     
     def findPolygon(self, image):
-         #Waehle Bildbereich zum Betrachten aus
+        
+        top = 780
+        bottom = 800
+        left = 226
+        right = 246
         image = cv2.cvtColor(image, cv2.cv.CV_GRAY2RGB)
-        roi = image[760:800,326:366]
-        #Summiere alle Pixel in diesem Bereich: summe[0] = 0 entspricht alles schwarz
-        summe = cv2.sumElems(roi)
-        print summe[0]
-        if(summe[0]<=0.0):
-            cv2.rectangle(image,(326,800),(366,780),(0,255,0),3)
-        else:
-            cv2.rectangle(image,(326,800),(366,780),(0,0,255),-1)  
+        for x in range(0, 13):        
+            for y in range(0, 10):
+                #Waehle Bildbereich zum Betrachten aus
+                roi = image[top:bottom,left:right]
+                #Summiere alle Pixel in diesem Bereich: summe[0] = 0 entspricht alles schwarz
+                summe = cv2.sumElems(roi)
+                print summe[0]
+                if(summe[0]<=20.0):
+                    cv2.rectangle(image,(left,bottom),(right,top),(0,255,0),1) #rand
+                else:
+                    cv2.rectangle(image,(left,bottom),(right,top),(0,0,255),-1) #gefuellt
+                top = top - 40
+                bottom = bottom - 40
+            bottom = 800
+            top = 780
+            left = left + 20
+            right = right + 20
+          
         cv2.imshow("", image)
         pass
         
