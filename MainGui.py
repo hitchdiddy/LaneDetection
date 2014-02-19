e2472f2b63c419bbc9b007240e13b3a2db3822db
jkronen <jan@digitalbild-service.de>
2014-02-19 17:06:03 +0100

    PolygonFitting added

--- MainGui.py
+++ MainGui.py
@@ -5,9 +5,9 @@ import sys
 import glob
 import math
 from BevTransformation import BevTransformation
-
 from LineFitting import LineFitting
 from EdgeDetection import EdgeDetection
+from PolygonFitting import PolygonFitting
 from PyQt4 import QtCore, QtGui 
 
 from PyQt4.QtCore import QPoint, QTimer
@@ -79,8 +79,8 @@ class ImageWidget(QWidget):
     def __init__(self):
         super(ImageWidget, self).__init__()
        
-        data_path = '/homes/jannik/BVSiAB/RoadSegmentation_Tutorial'
-        #data_path = '/home/jan/Downloads/RoadSegmentation_Tutorial/'
+        #data_path = '/homes/jannik/BVSiAB/RoadSegmentation_Tutorial'
+        data_path = '/home/jan/Downloads/RoadSegmentation_Tutorial/'
         load_dir_images = 'images/'
         load_dir_groundTruth = 'ground_truth/'
         data_dir = 'data/'
---
+++ PolygonFitting.py
@@ -0,0 +1,7 @@
+import cv2
+
+class PolygonFitting:
+    
+     def __init__(self):
+        pass
+        
