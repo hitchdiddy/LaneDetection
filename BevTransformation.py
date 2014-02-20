18e7466d69dc8e3cd6afd7be788f4dcd4a1b5576
jkronen <jan@digitalbild-service.de>
2014-02-20 13:19:24 +0100

    Removed unused data

--- BevTransformation.py
+++ BevTransformation.py
@@ -13,35 +13,7 @@ class BevTransformation:
     def setAmount(self, val):
         self.amount = val
         
-    def computeBev(self,image):
-        y = 0 #Hoehe
-        #Calibration data
-        trans = matrix([[0.064204210331161901], [-1.1659632805953182],[ 1.9516283622638129]])
-        R  = matrix([[1.0003006126397809, 0.0027500404928677445, 0.0076948745820731171],[-0.0029378803282461181, 1.0000292823888794, 0.024511264068503628],[-0.007625134538036143, -0.024533048818819892, 1.000003988608658]])
-        T  = matrix([[1,0,0, trans[0]],[0,1,0,trans[1]],[0,0,1,trans[2]],[0,0,0,1]]) 
-        
-        #Image data
-        t_u = 4.4/1000000.0*1600/800 #horizontal size of 1 pixel
-        t_v = 4.4/1000000.0*1200/600 #vertical size of 1 pixel
-        
-        f = matrix([[459 * 2 * t_v],[459 * t_u]]) #f=[fx, fy] im mm
-        f_px = matrix([[918],[918]])
-        center = matrix([[185.2133],[142.4146]])
-        u_0 = center[0] * 2
-        v_0 = center[1] * 2
-        
-        #BEV data
-        res = 1
-        xBounds = matrix([[-10],[10]])
-        zBounds = matrix([[0],[50]])
-        
-        #Metrische Koordinatesystem
-        zVec = range(zBounds[0],zBounds[1],res)
-        xVec = range(xBounds[0],xBounds[1],res)
-        
-        
-       
-        
+    def computeBev(self,image):      
         src = np.array([[350,131],[413,131],[75,325], [635,325]], np.float32)
         dst = np.array([[310,self.amount],[370,self.amount],[310,800], [370,800]], np.float32)
         #src = np.array([[348,251],[414,251],[224,316], [539,316]], np.float32)
@@ -60,4 +32,8 @@ class BevTransformation:
            
         
         return newimg
+    
+    def fitLine(self, image):
+        
+        return image
         
