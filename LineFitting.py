4b0b5b8f560c33b49c50644d6f465a608a635c72
Ole Richter <info@olerichter.com>
2014-02-21 16:00:21 +0100

    GUI verändert, LineFitting angepasst

--- LineFitting.py
+++ LineFitting.py
@@ -3,16 +3,17 @@ import numpy as np
 import math
 
 class LineFitting:
-    patchsizex = 100
-    patchsizey = 25
-    rho = 1
-    theta = np.pi/180
-    threshold = 60
+
+
+    
     #dataBlack = ""
     def __init__(self):
         self.rho = 1
         self.theta = np.pi/180
         self.threshold = 60
+        self.minLength = 100
+        self.patchsizex = 400
+        self.patchsizey = 200
         #self.dataBlack = dataBlack
         pass
     def setRho(self,rho1):
@@ -21,6 +22,10 @@ class LineFitting:
         self.theta = theta1
     def setThreshold(self,threshold1):
         self.threshold = threshold1
+    def setMinLength(self,minLength1):
+        
+        self.minLength = minLength1*min(self.patchsizex,self.patchsizey)/100.0
+        return self.minLength
       
     """def drawLine(self,rho,theta,img,dx,dy):
         a = math.cos(theta)
@@ -44,18 +49,7 @@ class LineFitting:
         for y in range(0, int(height/self.patchsizey)):
             for x in range(0, int(width/self.patchsizex)):
                 subimg = image[(x*self.patchsizex):(x*self.patchsizex+self.patchsizex),(y*self.patchsizey):(y*self.patchsizey+self.patchsizey)]
-                vector = cv2.HoughLinesP(subimg,self.rho, self.theta, self.threshold)
-                if(vector!=None):
-                    end = (vector.size/4)-1
-                    #for i2 in range(0,end):
-                    i2 = 0
-                    for x1,y1,x2,y2 in vector[i2]:
-                            #cv2.imshow('test3', subimg)
-                            cv2.line(imageBlack,(y*self.patchsizey+x1,x*self.patchsizex+y1),(y*self.patchsizey+x2,x*self.patchsizex+y2),(255,255,255),2)
-                        #cv2.line(subimg,(x1,y1),(x2,y2),(255,255,255),2)
-                        #plt.imshow(subimg)
-                        #cv2.imshow('test2', subimg)
-                        #cv2.waitKey(0)
+                imageBlack = self.findInPatch(subimg,imageBlack,x,y)
                 """vector = cv2.HoughLines(subimg, self.rho, self.theta, self.threshold)
                 if(vector!=None):
                     print(vector)
@@ -65,3 +59,53 @@ class LineFitting:
         
         return imageBlack #listVectors.pop()
     
+    def findInPatch(self,subimg, imageBlack,x,y):
+        vector = None
+        vector = cv2.HoughLinesP(subimg,self.rho, self.theta, self.threshold, vector,self.minLength)
+        if(vector!=None):
+            end = (vector.size/4)-1
+            #for i2 in range(0,end):
+            i2 = 0
+            for x1,y1,x2,y2 in vector[i2]:
+                    #cv2.imshow('test3', subimg)
+                    cv2.line(imageBlack,(y*self.patchsizey+x1,x*self.patchsizex+y1),(y*self.patchsizey+x2,x*self.patchsizex+y2),(255,255,255),2)
+        return imageBlack
+    
+if __name__ == '__main__':
+    
+    
+    line = LineFitting()
+    
+    
+    cv2.namedWindow('edge')
+    #cv2.createTrackbar('thrs1', 'edge', 2000, 10000, nothing)
+    #cv2.createTrackbar('thrs2', 'edge', 3000, 10000, nothing)
+    #cv2.createTrackbar('thrs3', 'edge', 0, 10, nothing)
+  
+    
+    
+    #print("Testfunction for Edge-Detection")
+    subimg = cv2.imread('test1.png')
+    subimg = cv2.cvtColor(subimg, cv2.cv.CV_BGR2GRAY)
+    imageBlack = np.zeros((400,400),np.uint8)
+    line.findInPatch(subimg, imageBlack,0,0)
+    cv2.imshow('', imageBlack)
+    cv2.imshow('2', subimg)
+    cv2.waitKey(0)
+    pass
+"""
+    
+    while True:
+        edgeDet.minTresh = cv2.getTrackbarPos('thrs1', 'edge')
+        edgeDet.maxTresh = cv2.getTrackbarPos('thrs2', 'edge')
+        edgeDet.sobel = cv2.getTrackbarPos('thrs3', 'edge')
+        edge = edgeDet.computeEdges(img)
+        vis = edge.copy()
+        vis /= 2
+     
+        cv2.imshow('edge', vis)
+        ch = cv2.waitKey(5)
+        if ch==27:
+            break
+      
+    cv2.destroyAllWindows()"""
